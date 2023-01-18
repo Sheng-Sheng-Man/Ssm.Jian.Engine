121735b7cf7a76446d5a5b6875db4c426c2c81ad
inmount <inmount@live.cn>
2023-01-19 07:54:51 +0800

    修改：兼容新版本的Lark引擎

---
+++ JianConsole/Functions/SystemFuncs.cs
@@ -0,0 +1,39 @@
+﻿using Egg.Lark;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JianConsole.Functions
+{
+    /// <summary>
+    /// 系统函数
+    /// </summary>
+    public class SystemFuncs
+    {
+        [Func("大于")]
+        public bool GreaterThan(double value1, double value2)
+        {
+            return value1 > value2;
+        }
+
+        [Func("大于等于")]
+        public bool GreaterThanOrEqual(double value1, double value2)
+        {
+            return value1 > value2;
+        }
+
+        [Func("小于")]
+        public bool LessThan(double value1, double value2)
+        {
+            return value1 < value2;
+        }
+
+        [Func("小于等于")]
+        public bool LessThanOrEqual(double value1, double value2)
+        {
+            return value1 <= value2;
+        }
+    }
+}
